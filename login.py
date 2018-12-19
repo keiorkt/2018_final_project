c41bc3cf70fd03fa7bddde20a2dbd7a5760d7264
reod0424 <45986872+reod0424@users.noreply.github.com>
2018-12-19 15:13:16 +0900

    Update login.py

--- login.py
+++ login.py
@@ -17,18 +17,7 @@ class Login:
     if attempts == 0:
       print("Your account is locked please contact the support center")
       raise 'Account authentication failed'
-    print(f"{attemps} attemps left")
     self.read_account_csv()
-<<<<<<< HEAD
-    print("Hello, plase enter your user ID")
-    self.user_id = input()
-    if self.user_id not in self.accounts.keys():
-      self.ask_user_name(attemps-1)
-    print("Hello, plase enter your user name")
-    self.user_name = input()
-    if self.user_name != self.accounts[self.user_id]['user_name']:
-      self.ask_user_name(attemps-1)
-=======
     print(f"{attempts} attempts left : Hello, plase enter your user ID")
     self.user_id = input()
     if self.user_id not in self.accounts.keys():
@@ -39,7 +28,6 @@ class Login:
     if self.user_name != self.accounts[self.user_id]['user_name']:
       print("Invalid user_name please enter the correct user_name")
       self.ask_user_name(attempts-1)
->>>>>>> 55e26a03af0d33a11dcfca3d14d233a77f77bba1
     self.validate_password(5)
 
   def read_account_csv(self):
@@ -49,7 +37,6 @@ class Login:
       next(reader, None)
       for line in reader:
         account = {}
-        
         account['user_name'] = line[1]
         account['pw'] = line[2]
         accounts[line[0]] = account
