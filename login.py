4aadff9badc1f8742b95ca36ce295127b9a1a277
Keita Iwasaki <keitaiwasaki@KTS-MBP.local>
2018-12-19 15:08:53 +0900

    change attempts

--- login.py
+++ login.py
@@ -2,33 +2,32 @@ import csv
 
 # このクラスを使用すればログインをすることができる
 class Login:
-  def validate_password(self, attemps):
-    print(f"{self.user_name} : please enter your password")
-    print(f"{attemps} attemps left")
+  def validate_password(self, attempts):
+    print(f"{attempts} attempts left : {self.user_name} please enter your password")
     password = input()
     if password ==  self.accounts[self.user_id]['pw']:
-      print("Logged in")
-    elif attemps == 0:
+      print("Logged in!!!")
+    elif attempts == 0:
       print("Your account is locked please contact the support center")
       raise 'Account authentication failed'
     else:
-      self.validate_password(attemps-1)
+      self.validate_password(attempts-1)
 
-  def ask_user_name(self, attemps):
-    if attemps == 0:
+  def ask_user_name(self, attempts):
+    if attempts == 0:
       print("Your account is locked please contact the support center")
       raise 'Account authentication failed'
     self.read_account_csv()
-    print(f"{attemps} attemps left : Hello, plase enter your user ID")
+    print(f"{attempts} attempts left : Hello, plase enter your user ID")
     self.user_id = input()
     if self.user_id not in self.accounts.keys():
       print("Invalid user_id please enter the correct one")
-      self.ask_user_name(attemps-1)
-    print(f"{attemps} attemps left : Hello, plase enter your user name")
+      self.ask_user_name(attempts-1)
+    print(f"{attempts} attempts left : Hello, plase enter your user name")
     self.user_name = input()
     if self.user_name != self.accounts[self.user_id]['user_name']:
       print("Invalid user_name please enter the correct user_name")
-      self.ask_user_name(attemps-1)
+      self.ask_user_name(attempts-1)
     self.validate_password(5)
 
   def read_account_csv(self):
