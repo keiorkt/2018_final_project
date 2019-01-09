1110d1827100e39962375903acb00f3f962b5d58
Keita Iwasaki <keitaiwasaki@KTS-MBP.local>
2019-01-09 15:56:33 +0900

    add two files

--- maya_show_attendance.py
+++ maya_show_attendance.py
@@ -10,5 +10,23 @@ student_list = {1: {"student_id" : "id_1", "name": "sample_1","attendance" : [Tr
                 3: {"student_id" : "id_3", "name": "sample_3", "attendance" : [True, True, True]}}
 
 # Task：一人一人の生徒の出席数、欠席数を表示するプログラミングを記述せよ
+
 # 補足：True = 出席、False = 欠席
 # 遅刻はあとで実装するので今は考えなくて良い
+
+# True= Total number classes attended by particular Student
+
+def display_total_attendance(student_list):
+    for i in range(len(student_list)):
+        i += 1
+        attend_count = student_list[i]["attendance"].count(True)
+        absent_count = student_list[i]["attendance"].count(False)
+        print("ID: " + student_list[i]["student_id"] + ", "
+              "Name: " + student_list[i]["name"] + ", "
+              "Attendance: " + str(attend_count) + ", "
+              "Absence: " + str(absent_count)
+              )
+    
+display_total_attendance(student_list)
+
+# student id: 1, name: "sample_1", attend: 2, absent: 1
--- usei_take_roll.py
+++ usei_take_roll.py
@@ -10,3 +10,17 @@ student_list = {1: {"student_id" : "id_1", "name": "sample_1", "attendance" : [T
                 3: {"student_id" : "id_3", "name": "sample_3", "attendance" : [True, True, True]}}
 
 # Task：一人一人の生徒の出欠を入力できるプログラミングを記述
+#4回目の授業の出席を取る
+
+def take_attendance(student_list):
+    for i in range(len(student_list)):
+        print("Is " + student_list[i+1]["name"] + " here? (yes/no)")
+        attendance_4 = input().lower()
+        if attendance_4 == "yes":
+            student_list[i+1]["attendance"].append(True)
+        elif attendance_4 == "no":
+            student_list[i+1]["attendance"].append(False)
+    print(student_list)
+
+
+take_attendance(student_list)
